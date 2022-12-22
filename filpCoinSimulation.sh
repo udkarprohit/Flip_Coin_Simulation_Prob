e4d85a47fe169f6223064d5f560cf0d0c3bd06ae
Prohit <udkarprohit@gmail.com>
2022-12-22 08:06:50 +0530

    Added: flipping a coin through multiple time and dispalying head and tail has won

--- filpCoinSimulation.sh
+++ filpCoinSimulation.sh
@@ -1,13 +1,18 @@
 #!/bin/bash 
 
 #Use Case-1 Random Tail or heads  
-
-x=$((RANDOM%2));	#0 1 Random number
+count=0;
+	#0 1 Random number
 #echo $x;
-if [ $x -eq 0 ]
-then
-	echo "Head";
-elif [ $x -eq 1 ]
-then
-	echo "Tail";
-fi
+while [ $count -le 10 ]
+do
+	x=$((RANDOM%2));
+	if [ $x -eq 0 ]
+	then
+		echo "Head";
+	elif [ $x -eq 1 ]
+	then
+		echo "Tail";
+	fi
+	((count++));
+done
